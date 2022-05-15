0b127aa0c7f5cf2e74c16174ce8fd0354d969fff
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2022-05-15 18:41:20 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const path = require("path");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db.js");
@@ -15,6 +15,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
+//serve frontend
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import {createGoal} from '../features/goals/goalSlice.js'
 
 function GoalForm() {
+const [text, setText] = useState('')
+const dispatch = useDispatch()
+
+
+
+  const onSubmit = (e)=> {
+    e.preventDefault()
+    dispatch(createGoal({text}))
+    setText('')
+  }
+
   return (
-    <div>GoalForm</div>
-  )
+    <section className="form">
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="text">Goal</label>
+          <input
+            type="text"
+            name="text"
+            value={text}
+            id="text"
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block" type="submit">Add Goal</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default GoalForm
+export default GoalForm;
