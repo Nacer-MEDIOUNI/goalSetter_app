08a818c561a955e783350f9c1c9327aac34beb69
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2022-05-15 19:05:32 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const express = require("express");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db.js");
